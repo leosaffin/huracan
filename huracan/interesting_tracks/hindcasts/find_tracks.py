d627edf06e18a89af98b9c800cfcf153880efc9e
Leo Saffin <string_buster@hotmail.com>
2024-02-26 09:32:52 +0000

    Put find_tracks in subfolder by dataset and add functions for loading specific tracks

--- huracan/interesting_tracks/hindcasts/find_tracks.py
+++ huracan/interesting_tracks/hindcasts/find_tracks.py
@@ -4,16 +4,7 @@ from storm_assess import track
 from storm_assess.regions import landfall_europe
 from storm_assess.functions.xarray_functions import storm_in_basin
 
-# The first date in the path is when the hindcast was run and the second is the
-# initialisation time. The hindcasts are run for all years back to 1995, initialised
-# for the same month/year they are run, so only the year is different in the path
-YYYYMMDDHH = "{year:04d}{month:02d}{day:02d}{hour:02d}"
-YYYYMMDDHH_model = YYYYMMDDHH.replace("year", "model_year")
 parent_path = pathlib.Path("/gws/nopw/j04/huracan/data/tracks/tropical_cyclones/TRACK/ECMWF-HINDCASTS/TC/")
-path = (
-    str(parent_path) + f"/{YYYYMMDDHH_model}/{YYYYMMDDHH}/" +
-    f"HIND_VOR_VERTAVG_{YYYYMMDDHH_model}_{YYYYMMDDHH}_" + "{ensemble_member}/"
-)
 filename = "tr_trs_pos.2day_addwinds_addmslp.highres.new"
 
 variable_names = [
