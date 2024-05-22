1627de65c2ed8552fcc096bf280093c685bdd6f7
Leo Saffin <string_buster@hotmail.com>
2024-05-22 10:32:11 +0100

    Switch to huracanpy

--- huracan/interesting_tracks/hindcasts/find_tracks.py
+++ huracan/interesting_tracks/hindcasts/find_tracks.py
@@ -7,13 +7,12 @@ from scipy.ndimage import uniform_filter1d
 from iris.analysis.cartography import wrap_lons
 from tqdm import tqdm
 
-from storm_assess import track, regions
-from storm_assess.functions.xarray_functions import storm_in_basin
+import huracanpy
 from jasmin_tracks import datasets
 
 
 def add_category(storm, filter_size=None):
-    storm["category"] = ("time", np.zeros(len(storm.time), dtype="<U3"))
+    storm["category"] = ("obs", np.zeros(len(storm.time), dtype="<U3"))
 
     b = np.abs(storm.B)
     vtu = storm.TU
@@ -57,28 +56,33 @@ def find_tracks():
     ))
 
     for fname in tqdm(all_files):
-        tracks = track.load_no_assumptions(str(fname), variable_names=dataset.variable_names)
+        tracks = huracanpy.load(str(fname), tracker="TRACK", variable_names=dataset.variable_names)
         details = dataset.file_details(str(fname))
         start_time = datetime.datetime(
             **{key: details[key] for key in ["year", "month", "day", "hour"]}
         )
-        for tr in tracks:
+        for n, tr in tracks.groupby("track_id"):
             # Only North Atlantic storms
-            if storm_in_basin(tr, "na"):
+            basin = huracanpy.utils.geography.get_basin(tr.lon, tr.lat)
+            if (basin == "NATL").any():
                 # Only data every 12 hours for CPS in hindcasts
                 times = pd.to_datetime(tr.time)
-                times_ = [t for t in times if t.hour % 12 == 0]
-                tr_ = tr.sel(time=times_)
+                times_ = [t for t in tr.time.data if pd.to_datetime(t).hour % 12 == 0]
+                tr_ = tr.where(tr.time.isin(times_), drop=True)
+                basin = huracanpy.utils.geography.get_basin(tr_.lon, tr_.lat)
 
                 # Only storms that are tropical cyclones at some point in their lifecycle
                 add_category(tr_, filter_size=3)
+                # Only include the categories in the North Atlantic basin
+                tr_["category"][basin != "NATL"] = "NC"
+
                 category_consecutive = [(k, sum(1 for i in g)) for k, g in groupby(tr_.category.data)]
                 tcident = False
                 for category, count in category_consecutive:
                     if category == "TC" and count > 1:
                         tcident = True
                 if tcident:
-                    in_tropics = tr.latitude < 30
+                    in_tropics = tr.lat < 30
                     if in_tropics.any():
                         min_pressure_tropics = tr.mslp[in_tropics].data.min()
                         max_intensity_tropics = tr.vmax10m[in_tropics].data.max()
@@ -87,8 +91,8 @@ def find_tracks():
                         max_intensity_tropics = np.nan
 
                     # 36–70 deg N and 10 deg W–30 deg E
-                    lons = wrap_lons(tr.longitude, -180, 360)
-                    in_europe = (-10 < lons) & (lons < 30) & (36 < tr.latitude) & (tr.latitude < 70)
+                    lons = wrap_lons(tr.lon, -180, 360)
+                    in_europe = (-10 < lons) & (lons < 30) & (36 < tr.lat) & (tr.lat < 70)
                     if in_europe.any():
                         min_pressure_europe = tr.mslp[in_europe].data.min()
                         max_intensity_europe = tr.vmax10m[in_europe].data.max()
@@ -101,18 +105,19 @@ def find_tracks():
                         ensemble_member=details["ensemble_member"],
                         storm_start=times[0],
                         storm_end=times[-1],
-                        origin_lat=tr.latitude.data[0],
-                        origin_lon=tr.longitude.data[0],
-                        end_lat=tr.latitude.data[-1],
-                        end_lon=tr.longitude.data[-1],
+                        origin_lat=tr.lat.data[0],
+                        origin_lon=tr.lon.data[0],
+                        end_lat=tr.lat.data[-1],
+                        end_lon=tr.lon.data[-1],
                         min_pressure_tropics=min_pressure_tropics,
                         max_intensity_tropics=max_intensity_tropics,
                         min_pressure_europe=min_pressure_europe,
                         max_intensity_europe=max_intensity_europe,
                     )])], ignore_index=True)
 
-    summary.to_csv("interesting_tracks_ECMWF_Hindcasts_2015.csv")
+    summary.to_csv("interesting_tracks_ECMWF_Hindcasts_2015_test.csv")
 
 
 if __name__ == '__main__':
     find_tracks()
+
